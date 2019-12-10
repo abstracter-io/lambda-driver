6cbb9d50c0dd9bd4a72c61529895361566d7807f
Elad Chen <eladchen@gmail.com>
2019-12-10 11:23:18 +0200

    Fixup

--- src/main/java/io/abstractor/lambda/runtime/port/ParameterSerializer.java
+++ src/main/java/io/abstractor/lambda/runtime/port/ParameterSerializer.java
@@ -1,11 +1,13 @@
 package io.abstractor.lambda.runtime.port;
 
 /**
- * A serialize that takes a parameter type (of a given method)
- * an execution context, and returns an object with the parameter value.
- *
- * @param <T> ExecutionContext type / sub-type
+ * @param <T> type / sub-type that implements ExecutionContext
  */
 public interface ParameterSerializer<T extends ExecutionContext> {
-    <R> R serialize(Class<?> type, T executionContext);
+    /**
+     * @param <R> The expected type of the serialized object
+     * @param cls A class reference of type R
+     * @param executionContext an execution context.
+     */
+    <R> R serialize(Class<R> cls, T executionContext);
 }
